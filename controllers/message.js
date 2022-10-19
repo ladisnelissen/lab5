df72badadd75dba744f217e6bb5afb7b9836095d
Ladis Nelissen <ladisnelissen@hotmail.com>
2022-10-19 17:49:44 +0200

    feat: add app.js template

--- controllers/message.js
+++ controllers/message.js
@@ -1,19 +1,17 @@
 const Message = require('../models/message');
 
 const getMessage = (req, res) => {
-    Message.find()
-        .populate('user', 'name')
-        .then(result => {
-            res.status(200).json({
-                messages: result
-            });
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        });
+    let messages = Message.find({}, (err, messages) => {
+        if (err) {
+            let result = {
+                status: 'error',
+                message: "error getting messages"
+            };
+            
+        } else {
+            res.status(200).send(messages);
+        }
+    });
 };
 
 const createMessage = (req, res) => {
