74330974e8111629780bd96ac88b00437f22c57f
Ladis Nelissen <ladisnelissen@hotmail.com>
2022-10-19 17:39:18 +0200

    build: run express

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+const port = 3000;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -39,3 +40,6 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
