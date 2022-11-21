d446bf69476b52bafc208693115f51f22143289f
Ladis Nelissen <ladisnelissen@hotmail.com>
2022-11-21 15:25:04 +0100

    build: add variables in app.js

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+var indexRouter = require('./routes/index');
+var messagesRouter = require('./routes/messages');
 
-app.use(express.json());
-app.use('/messages', require('./routes/message'))
+var app = express();
 
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
