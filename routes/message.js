7dca7bfa88a0646af7d9c8df350f69f517624aff
Ladis Nelissen <ladisnelissen@hotmail.com>
2022-11-21 15:14:40 +0100

    feat: define routes and use controller

---
+++ routes/message.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const messagesController = require('../controllers/messages');
+
+router.get('/', messagesController.getAll);
+router.get('/:id', messagesController.getById);
+
+router.post('/', messagesController.create);
+
+router.put('/:id', messagesController.update);
+
+router.delete('/:id', messagesController.deleteById);
+
+module.exports = router;
